bd7fe8dd94f5d749365380a7ed04e5808e18b5ea
Syakyr Surani <syakyr@aisingapore.org>
2024-08-07 10:59:58 +0800

    Format whitespaces in tf env templates

--- terraform/envs/gcp/main.tf
+++ terraform/envs/gcp/main.tf
@@ -1,84 +1,83 @@
 terraform {
-	backend "gcs" {}
+  backend "gcs" {}
 }
 
 locals {
-	gar_mlflow_repo	   = "asia-southeast1-docker.pkg.dev/machine-learning-ops/pub-images/mlflow-server:stable"
+  gar_mlflow_repo = "asia-southeast1-docker.pkg.dev/machine-learning-ops/pub-images/mlflow-server:stable"
 }
 
 provider "kubernetes" {
-	config_path = pathexpand(var.kubeconfig)
+  config_path = pathexpand(var.kubeconfig)
 }
 
 # generate secrets - runai-sso
 resource "kubernetes_secret" "runai-sso" {
-	metadata {
-		name	  = "runai-sso"
-		namespace = var.namespace
-	}
+  metadata {
+    name      = "runai-sso"
+    namespace = var.namespace
+  }
 
-	data = {
-		"runai-sso.yaml" = file(var.runai_kubeconfig)
-	}
+  data = {
+    "runai-sso.yaml" = file(var.runai_kubeconfig)
+  }
 }
 
 # GKE Secrets - gcp-sa-credentials 
 resource "kubernetes_secret" "gcp-credentials" {
-	metadata {
-		name	  = "gcp-sa-credentials"
-		namespace = var.namespace
-	}
+  metadata {
+    name      = "gcp-sa-credentials"
+    namespace = var.namespace
+  }
 
-	data = {
-		"gcp-service-account.json" = file(var.gcs_credentials)
-	}
+  data = {
+    "gcp-service-account.json" = file(var.gcs_credentials)
+  }
 }
 
 # create RWX PVC
 resource "kubernetes_persistent_volume_claim" "pvc-data-gke" {
-	metadata {
-		name	  = var.pvc_name
-		namespace = var.namespace
-	}
-	spec {
-		storage_class_name = "fs-std-rwx"
-		access_modes	   = ["ReadWriteMany"]
-		resources {
-			requests = {
-				storage = "1Ti"
-			}
-		}
-	}
-	timeouts {
-		create = "15m"
-	}
+  metadata {
+    name      = var.pvc_name
+    namespace = var.namespace
+  }
+  spec {
+    storage_class_name = "fs-std-rwx"
+    access_modes       = ["ReadWriteMany"]
+    resources {
+      requests = {
+        storage = "1Ti"
+      }
+    }
+  }
+  timeouts {
+    create = "15m"
+  }
 }
 
 module "mlflow-server" {
-	source				 = "../../modules/mlflow/"
-	backend_storage		 = "gcs"
-	artefact_bucket_name = var.artefact_bucket_name
-	namespace			 = var.namespace
-	pvc_name			 = var.pvc_name
-	custom_image		 = local.gar_mlflow_repo
-	gcp_project_id		 = var.gcp_project_id
-	kubeconfig			 = var.kubeconfig
-	ingress_hostname     = format("mlflow.%s", var.root_url)
-	node_selector_key    = var.node_selector_key
-	node_selector_value  = var.node_selector_value
+  source               = "../../modules/mlflow/"
+  backend_storage      = "gcs"
+  artefact_bucket_name = var.artefact_bucket_name
+  namespace            = var.namespace
+  pvc_name             = var.pvc_name
+  custom_image         = local.gar_mlflow_repo
+  gcp_project_id       = var.gcp_project_id
+  kubeconfig           = var.kubeconfig
+  ingress_hostname     = format("mlflow.%s", var.root_url)
+  node_selector_key    = var.node_selector_key
+  node_selector_value  = var.node_selector_value
 }
 
 module "coder-server" {
-	source				 = "../../modules/coder/"
-	kubeconfig			 = var.kubeconfig
-	namespace			 = var.namespace
-	coder_url			 = format("coder.%s", var.root_url)
-	node_selector_key    = var.node_selector_key
-	node_selector_value  = var.node_selector_value
-	auth_method			 = var.coder_auth
-	oidc_issuer_url		 = var.coder_auth == "oidc" ? var.oidc_issuer_url : null
-	oidc_email_domain	 = var.coder_auth == "oidc" ? var.oidc_email_domain : null
-	oidc_client_id		 = var.coder_auth == "oidc" ? var.oidc_client_id : null
-	oidc_client_secret	 = var.coder_auth == "oidc" ? var.oidc_client_secret : null
+  source               = "../../modules/coder/"
+  kubeconfig           = var.kubeconfig
+  namespace            = var.namespace
+  coder_url            = format("coder.%s", var.root_url)
+  node_selector_key    = var.node_selector_key
+  node_selector_value  = var.node_selector_value
+  auth_method          = var.coder_auth
+  oidc_issuer_url      = var.coder_auth == "oidc" ? var.oidc_issuer_url : null
+  oidc_email_domain    = var.coder_auth == "oidc" ? var.oidc_email_domain : null
+  oidc_client_id       = var.coder_auth == "oidc" ? var.oidc_client_id : null
+  oidc_client_secret   = var.coder_auth == "oidc" ? var.oidc_client_secret : null
 }
-
--- terraform/envs/onprem/variables.tf
+++ terraform/envs/onprem/variables.tf
@@ -1,88 +1,86 @@
 variable "namespace" {
-	type		= string
-	description = "Deployment namespace within the Kubernetes cluster"
+  type        = string
+  description = "Deployment namespace within the Kubernetes cluster"
 }
 
 variable "root_url" {
-	type		= string
-	description = "Root URL for Mlflow and Coder servers, without the resource header. e.g. 100e-exampleproj.aisingapore.net"
+  type        = string
+  description = "Root URL for Mlflow and Coder servers, without the resource header. e.g. 100e-exampleproj.aisingapore.net"
 }
 
 variable "artefact_bucket_name" {
-	type		= string
-	description = "Bucket name where artefacts will be stored"
+  type        = string
+  description = "Bucket name where artefacts will be stored"
 }
 
 variable "kubeconfig" {
-	type		= string
-	description = "Location of the kubeconfig of the target Kubernetes cluster"
+  type        = string
+  description = "Location of the kubeconfig of the target Kubernetes cluster"
 }
 
 variable "pvc_name" {
-	type		= string
-	description = "Name of the RWX Persistent Storage Claim to be created"
+  type        = string
+  description = "Name of the RWX Persistent Storage Claim to be created"
 }
 
 variable "runai_kubeconfig" {
-	type	    = string
-	description = "Path to the non-initialised kubeconfig for the runai cluster"
+  type        = string
+  description = "Path to the non-initialised kubeconfig for the runai cluster"
 }
 
 variable "ecs_access_key" {
-	type	    = string
-	description = "Access Key ID for ECS"
-	default		= ""
+  type        = string
+  description = "Access Key ID for ECS"
+  default     = ""
 }
 
 variable "ecs_secret_key" {
-	type	    = string
-	description = "Secret Key for ECS"
-	default		= ""
+  type        = string
+  description = "Secret Key for ECS"
+  default     = ""
 }
 
 variable "node_selector_key" {
-	type 		= string
-	description = "Key for node selector, required if GPU nodes present"
-	default     = ""
+  type        = string
+  description = "Key for node selector, required if GPU nodes present"
+  default     = ""
 }
 
 variable "node_selector_value" {
-	type 		= string
-	description = "Value for node selector, required if GPU nodes present"
-	default     = ""
+  type        = string
+  description = "Value for node selector, required if GPU nodes present"
+  default     = ""
 }
 
 variable "coder_auth" {
-	type		= string
-	description = "Authentication methods for Coder server, either 'oidc' or 'password'"
-	
-	validation {
-		condition	  = contains(["oidc", "password"], var.coder_auth)
-		error_message = "Invalid authentication method provided."
-	}
+  type        = string
+  description = "Authentication methods for Coder server, either 'oidc' or 'password'"
+  validation {
+    condition     = contains(["oidc", "password"], var.coder_auth)
+    error_message = "Invalid authentication method provided."
+  }
 }
 
 variable "oidc_issuer_url" {
-	type		= string
-	description = "URL for OIDC issuer, required if auth_method is set to 'oidc'"
-	default		= ""
+  type        = string
+  description = "URL for OIDC issuer, required if auth_method is set to 'oidc'"
+  default     = ""
 }
 
 variable "oidc_email_domain" {
-	type		= string
-	description = "Valid email domains for OIDC, required if auth_method is set to 'oidc'"
-	default		= ""
+  type        = string
+  description = "Valid email domains for OIDC, required if auth_method is set to 'oidc'"
+  default     = ""
 }
 
 variable "oidc_client_id" {
-	type		= string
-	description = "Client ID for OIDC, required if auth_method is set to 'oidc'"
-	default		= ""
+  type        = string
+  description = "Client ID for OIDC, required if auth_method is set to 'oidc'"
+  default     = ""
 }
 
 variable "oidc_client_secret" {
-	type		= string
-	description = "Client Secret for OIDC, required if auth_method is set to 'oidc'"
-	default		= ""
+  type        = string
+  description = "Client Secret for OIDC, required if auth_method is set to 'oidc'"
+  default     = ""
 }
-
