601c15403b4c4c34e980765f9d41b8e92902698d
Augustine Teo <augustine@aisingapore.org>
2024-11-06 16:05:49 +0800

    fix: redo potential fix for legacy module issue

--- terraform/modules/neo4j/variables.tf
+++ terraform/modules/neo4j/variables.tf
@@ -1,32 +1,6 @@
-variable "kubeconfig" {
-  type        = string
-  description = "Location of the cluster's kubeconfig file"
-}
-
 variable "namespace" {
   type        = string
   description = "Deployment namespace"
-}
-
-
-variable "neo4j_cluster_name" {
-  type        = string
-  description = "Name of your cluster"
-}
-
-variable "storage_mode" {
-  type = string
-  description = "REQUIRED: specify a volume mode to use for data. Valid values are share|selector|defaultStorageClass|volume|volumeClaimTemplate|dynamic"
-}
-
-variable "service_type" {
-  type = string
-  description = "Type of service to use. Use NodePort for deploying on GKE with Ingress"
-}
-
-variable "service_name" {
-  type = string
-  description = "Name of the kubernetes service. This service should have the ports 7474 and 7687 open."
 }
 
 variable "node_selector_key" {
